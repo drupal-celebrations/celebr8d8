443ec16b40cf349381912f635a3cf5af5012bb73
a.hover <a.hover@ctidigital.com>
2015-11-11 20:26:17 +0000

    Added new Jackdaw base theme. Implemented menu toggle. Finished Featured view header.

---
+++ themes/custom/celebr8d8/js/celebr8d8.behavior.menu-toggle.js
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * Responsive navigation toggle.
+ *
+ */
+(function ($, Drupal) {
+
+  /**
+   * Initialise the tabs JS.
+   */
+  Drupal.behaviors.menuToggle = {
+    attach: function (context, settings) {
+      var $header = $('.region-header'),
+        $nav = $('.region-navigation'),
+        menuOpenClass = 'toggle--menu--open';
+
+      // Function to close open menus.
+      function manualToggleMenu() {
+        if (matchMedia('(min-width: 960px)').matches) {
+          if ($('.toggle--menu').not('.toggle--menu--open').length) {
+            $nav.show();
+          }
+        }
+      }
+
+      // Add the menu toggle button.
+      $header.once()
+        .prepend('<a class="toggle toggle--menu" href="#">Toggle Menu</a>')
+        .find('.toggle--menu')
+        .click(function (e) {
+          e.preventDefault();
+
+          if ($(this).hasClass(menuOpenClass)) {
+            $(this).removeClass(menuOpenClass);
+            $nav.slideUp();
+          }
+          else {
+            $(this).addClass(menuOpenClass);
+            $nav.slideDown();
+          }
+        });
+
+      $(window).on('resize', Drupal.debounce(manualToggleMenu, 150)).trigger('resize');
+    }
+  };
+
+})(jQuery, Drupal);
